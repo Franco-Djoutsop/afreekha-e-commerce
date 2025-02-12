7ae54db7039e512ef65241a539fc5da6a60f64cb
Djoutsop Franck <francodjoutsoppro@gmail.com>
2025-02-12 07:51:55 +0100

    add route

--- backend/src/routes/clientRoute.ts
+++ backend/src/routes/clientRoute.ts
@@ -1,9 +1,34 @@
 import express, { Express } from "express";
-import { createUser } from "../controllers/userControler";
+import {
+  allUSers,
+  allUsersRoles,
+  createUser,
+  deleteUsers,
+  oneUsersRole,
+  updateUsers,
+} from "../controllers/userControler";
+import { userValidationRules } from "../middlewares/validationsRules";
+import { validate } from "../middlewares/validate";
+import {
+  allRoles,
+  createRole,
+  deleteRole,
+  roleUsers,
+  updateRole,
+} from "../controllers/roleControler";
 
 const router = express.Router();
 
-//client route
-router.route("/users").post(createUser);
+//users route
+router.route("/users").post(userValidationRules, validate, createUser);
+router.route("/users").get(allUSers);
+router.route("/usersRoles/:id").get(allUsersRoles);
+router.route("/usersRoles/:id").get(oneUsersRole);
+router.route("/users/:id").patch(updateUsers);
+router.route("/users/:id").delete(deleteUsers);
+
+//role route
+router.route("/role").post(createRole).get(allRoles);
+router.route("/role/:id").patch(updateRole).delete(deleteRole);
 
 export default router;
