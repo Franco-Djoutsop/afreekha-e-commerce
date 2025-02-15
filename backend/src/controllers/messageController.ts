0ebbeb9cd55effb5434eb05d6126a9ca2b06cbb4
Fidele2j <djeukamfidele24@gmail.com>
2025-02-16 00:31:21 +0100

    gestion des messages

---
+++ backend/src/controllers/messageController.ts
@@ -0,0 +1,114 @@
+import User from "../models/User";
+import message from '../models/Message';
+import { Request, Response } from 'express';
+import { crypt } from "../config/crypto-js";
+
+const gest_message ={
+      
+//@route/api/message
+//@mathod post
+//@data = objet, response true ? false
+    
+//envoi d'un message    
+async  createMessage(req: Request,res:Response){
+        try{
+            const data = req.body;
+    
+            if(data != null){
+                await message.create({
+                    'idUser': data.idUser,
+                    'contenus': data.contenus,
+                });
+              return  res.status(201).json({
+                'send': true,
+                'message': 'message envoyez avec sucess!!'
+            });
+            } 
+            return res.status(404).json({message:'veuillez fournir les donnees'});
+           
+        }catch(error){
+           return res.status(500).json({ 
+            'message' : "echec de l'envoi",
+            'erreur': console.log(error)
+    });
+        }   
+    },
+    
+//@route /api/admin/message
+//@method delete
+//@response true ? false
+
+//suppression dun message
+
+async deleteMessage(req:Request,res:Response){
+    try{
+       let id = req.params.id
+       const checkid = await message.findByPk(id);
+       if(!checkid){
+        return res.status(404).json({
+            "message":"aucun message trouve"
+        });
+       }
+       await message.destroy({
+        where:{
+            'idMessage': id
+        }
+       });
+       return res.status(200).json({
+        "message":"suppression reussi"
+       });
+       
+    }catch(error){
+      console.log(error);
+      res.status(500).json({
+        "message":"erreur sur le serveur"
+      });
+    }
+
+},
+
+//@route /api/admin/detail-message
+//@method get
+//@response true ? false
+
+//liste des message d'un utilisateur
+
+async getMessage(req:Request, res:Response){
+    try{
+        const id = req.params.id;
+        const information = await message.findAll(
+            {  
+                where:
+                {
+                    idUser:id,       
+                },
+            
+                include:[
+                    { 
+                        model:User, attributes:['idUser','nom','prenom','tel']
+                    },
+                ],
+                attributes:['contenus'],
+            }
+        )
+        
+        if(information[0]==null){
+            return res.status(404).json({
+                'data':[],
+                'message':'aucun message trouve pour ce client'
+            });
+        }
+            return res.status(200).json({
+                'data': crypt.encode(information)
+             })
+       
+    }catch(error){
+       console.log(error);
+       return res.status(500).json({
+        'message':'erreur du serveur'
+       })
+    }
+}
+
+}
+ export default gest_message;
